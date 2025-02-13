83c1e6e7868a99a28d39b61225c25d16ad46a9a0
Siddartha Yadav Sangam Reddy <siddhuyadav508@gmail.com>
2025-02-14 02:55:21 +0530

    Added changes to App.ts,Error.ts,CAE.ts and to package.json

--- server/app.ts
+++ server/app.ts
@@ -3,6 +3,7 @@ import express, { NextFunction, Request, Response } from "express";
 export const app = express();
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import {ErrorMiddleware} from "./middleware/error";
 
 // body parser
 app.use(express.json({ limit: "50mb" }));
@@ -31,3 +32,6 @@ app.all("*", (req: Request, res: Response, next: NextFunction) => {
     err.statusCode = 404;
     next(err);
 });
+
+// error handling middleware
+app.use(ErrorMiddleware);
---
+++ server/utils/ErrorHandler.ts
@@ -0,0 +1,16 @@
+interface ErrorConstructor {
+  captureStackTrace(targetObject: Object, constructorOpt?: Function): void;
+}
+
+class ErrorHandler extends Error {
+  statusCode: Number;
+
+  constructor(message: any, statusCode: Number) {
+    super(message);
+    this.statusCode = statusCode;
+
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+export default ErrorHandler;
